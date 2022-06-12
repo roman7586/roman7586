b67b3f6df41efbe0c7a6211128f9110be559d0ef
Roman7586 <roman7586@gmail.com>
2022-06-12 17:00:03 +0300

    делаем 6.2 с отдельным файлом сигналс.

--- pythonProject/Dtotal/NewsPaper/news/signals.py
+++ pythonProject/Dtotal/NewsPaper/news/signals.py
@@ -3,7 +3,7 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 
-from .models import *
+from .models import Post, Category
 
 
 @receiver(m2m_changed, sender=Post.postCategory.through)
