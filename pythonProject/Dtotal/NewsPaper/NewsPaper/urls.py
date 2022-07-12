80b00b22f89db2203c3a31aba9de8fdaad6e1a13
roman7586 <1xcanonx1>
2022-07-12 22:15:55 +0300

    делаем D14 модуль. Поле с выбором языка, но не переводятся Контакты, О, главная

--- pythonProject/Dtotal/NewsPaper/NewsPaper/urls.py
+++ pythonProject/Dtotal/NewsPaper/NewsPaper/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     # Делаем так, чтобы все адреса из нашего приложения (news/urls.py)
