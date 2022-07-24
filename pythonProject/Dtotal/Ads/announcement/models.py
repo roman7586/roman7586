784262e32940ec5015c9e40c63cdcc79d9cb36cb
roman7586 <1xcanonx1>
2022-07-24 19:13:53 +0300

    Проект Обьявления/ Добавил поля в модель с загрузкой файлов

--- pythonProject/Dtotal/Ads/announcement/models.py
+++ pythonProject/Dtotal/Ads/announcement/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Create your models here.
 
+def author_directory_path(instance, filename):
+    return 'author_{0}/{1}'.format(instance.author.id, filename)
+
 class Author(models.Model):
     authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -26,8 +29,16 @@ class Post(models.Model):
     )
     dateCreation = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=128)
-    category = models.CharField(max_length=2, choices=TYPE, default='tanks')
+    category = models.CharField(max_length=11, choices=TYPE, default='tanks')
     text = models.TextField()
+    content=models.FileField(upload_to=author_directory_path)
 
     #postCategory = models.ManyToManyField(Category, through='PostCategory')
     #rating = models.SmallIntegerField(default=0)
+
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
