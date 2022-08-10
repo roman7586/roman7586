e9d5ad58a3804bb7cbfb343b979485699e15db69
roman7586 <1xcanonx1>
2022-08-10 22:22:02 +0300

    Проект Обьявления. переписание

--- pythonProject/Dtotal/advt/advtpost/advert/urls.py
+++ pythonProject/Dtotal/advt/advtpost/advert/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     #path('indexx/', IndexView.as_view()),
     #path('indexx/upgrade/', upgrade_me, name = 'upgrade')
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
